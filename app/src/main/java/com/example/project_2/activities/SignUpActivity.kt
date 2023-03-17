5416403f49800cb86af9c421a576fc6cc8c77e90
rishabh.singh <rishabhsingh69532gmail.com>
2023-03-17 18:54:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/project_2/activities/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project_2.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.project_2.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
